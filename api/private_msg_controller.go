136ca2d848ac095f40bf5e0d8f1b896eae5a7ccb
miraclew <miraclew@gmail.com>
2015-03-26 08:44:44 +0800

    lots

--- api/private_msg_controller.go
+++ api/private_msg_controller.go
@@ -4,8 +4,8 @@ import (
 	"coding.net/miraclew/pusher/pusher"
 	"coding.net/miraclew/pusher/restful"
 	"encoding/json"
-	"github.com/miraclew/mrs/util"
-	"strconv"
+	"sort"
+	// "strconv"
 )
 
 type PrivateMsgController struct {
@@ -15,25 +15,30 @@ type PrivateMsgController struct {
 func (this *PrivateMsgController) Post() {
 	values := this.Request.PostForm
 
-	typ, _ := strconv.ParseInt(values.Get("type"), 0, 64)
+	//typ, _ := strconv.ParseInt(values.Get("type"), 0, 64)
 
 	var payload interface{}
 	err := json.Unmarshal([]byte(values.Get("payload")), payload)
 	if err != nil {
-		return this.Fail(403, nil)
+		this.Fail(403, nil)
+		return
 	}
 
 	senderId := values.Get("sender_id")
 	receiverId := values.Get("receiver_id")
-	members := util.SplitUniqSort([]string{senderId, receiverId})
-	if senderId == nil || receiverId == nil || len(members) < 2 {
-		return this.Fail(ERR_INVALID_ARGS, nil)
+	members := sort.StringSlice{senderId, receiverId}
+	members = sort.StringSlice(members)
+	members.Sort()
+	if senderId == "" || receiverId == "" || len(members) < 2 {
+		this.Fail(ERR_INVALID_ARGS, nil)
+		return
 	}
 
 	var channel *pusher.Channel
 	channel, err = pusher.GetChannelByMembers(members)
 	if err != nil {
-		return this.Fail(ERR_INTERAL_ERROR, nil)
+		this.Fail(ERR_INTERAL_ERROR, nil)
+		return
 	}
 
 	options := map[string]interface{}{
@@ -41,7 +46,7 @@ func (this *PrivateMsgController) Post() {
 	}
 	m := pusher.NewMessage(
 		channel.Id,
-		MESSAGE_TYPE_NORMAL,
+		pusher.MESSAGE_TYPE_NORMAL,
 		payload,
 		senderId,
 		options,
--- pusher/hub.go
+++ pusher/hub.go
@@ -1,1 +1,176 @@
 package pusher
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+)
+
+const (
+	TYPE_PRIVATE = 1
+	TYPE_GROUP   = 2
+	TYPE_BULK    = 3
+)
+
+type Connection interface {
+	SendUTF([]byte) error
+}
+
+type Hub struct {
+	connections map[int64]Connection
+}
+
+func (h *Hub) AddConnection(userId int64, conn Connection) {
+	h.connections[userId] = conn
+}
+
+func (h *Hub) RemoveConnection(userId int64) {
+	delete(h.connections, userId)
+}
+
+func (h *Hub) pushMsg(msg *Message) {
+	log.Printf("pushMsg: %#v\n", msg)
+	if msg.ChannelId == "0" {
+		h.broadcast(msg, true)
+		return
+	}
+
+	if msg.Type == TYPE_PRIVATE || msg.Type == TYPE_GROUP {
+
+	}
+}
+
+func (h *Hub) broadcast(msg *Message, online bool) {
+	for _, v := range h.connections {
+		payload, _ := json.Marshal(msg.Payload)
+		log.Println(payload, v)
+		v.SendUTF(payload)
+	}
+}
+
+func (h *Hub) toUsers(msg *Message, users []int64) error {
+	skipSender := true
+	for i := 0; i < len(users); i++ {
+		u := users[i]
+		senderId, err := strconv.ParseInt(msg.SenderId, 10, 64)
+		if err != nil {
+			log.Println("Warn: msg senderId is not integer", msg)
+			continue
+		}
+		if u == senderId && skipSender {
+			log.Println("skipSender: ", u)
+			continue
+		}
+
+		// push to queue
+	}
+
+	return nil
+}
+
+func (h *Hub) sendToUser(userId int64, msg *Message) error {
+	conn, ok := h.connections[userId]
+	if ok {
+		// send to user
+		payload, _ := json.Marshal(msg.Payload)
+		err := conn.SendUTF(payload)
+		if err != nil {
+			return err
+		}
+	} else {
+		h.pushToQueue(userId, msg, false)
+	}
+
+	return nil
+}
+
+func (h *Hub) pushToQueue(userId int64, msg *Message, left bool) (length int, err error) {
+	cmd := "rpush"
+	if left {
+		cmd = "lpush"
+	}
+
+	res := redis.Cmd(cmd, fmt.Sprintf("mq:%d", userId), msg.Id)
+	if res.Err != nil {
+		log.Println(res.Err)
+		return -1, res.Err
+	}
+
+	return res.Int()
+}
+
+func (h *Hub) processQueue(userId int64) (length int, err error) {
+	res := redis.Cmd("rpop", fmt.Sprintf("mq:%d", userId))
+	if res.Err != nil {
+		log.Println(res.Err)
+		return -1, res.Err
+	}
+
+	var msgId string
+	msgId, err = res.Str()
+	if err != nil {
+		return -1, err
+	}
+
+	var msg *Message
+	msg, err = FindMessage(msgId)
+
+	if err != nil {
+		return -1, err
+	}
+
+	if msg == nil {
+		return -1, errors.New(fmt.Sprintf("msgId: %s not found", msgId))
+	}
+
+	err = h.sendToUser(userId, msg)
+	if err != nil {
+		return -1, err
+	}
+
+	res = redis.Cmd("llen", fmt.Sprintf("mq:%d", userId))
+	if res.Err != nil {
+		log.Println(res.Err)
+		return -1, res.Err
+	}
+
+	return res.Int()
+}
+
+func (h *Hub) pushToIosDevice(userId int64, msg *Message, length int) error {
+	res := redis.Cmd("get", fmt.Sprintf("apn_u2t:%d", userId))
+	if res.Err != nil {
+		log.Println(res.Err)
+		return res.Err
+	}
+
+	if deviceToken, _ := res.Str(); deviceToken != "" {
+		// TODO: push to iOS device
+	} else {
+		log.Printf("user: %d offline, and has no apns device token\n", userId)
+	}
+
+	return nil
+}
+
+func (h *Hub) toChannel(msg *Message, channelId string) error {
+	log.Println("push to channel_id:", channelId)
+
+	res := redis.Cmd("smembers", fmt.Sprintf("cm:%s", channelId))
+	if res.Err != nil {
+		log.Println(res.Err)
+		return res.Err
+	}
+
+	ls, _ := res.List()
+
+	var users []int64
+	for _, v := range ls {
+		userId, _ := strconv.ParseInt(v, 10, 64)
+		users = append(users, userId)
+	}
+
+	return h.toUsers(msg, users)
+}
