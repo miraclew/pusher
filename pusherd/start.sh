a2dc41cbedf1c529e62ed6d6119bc17cc380b3d0
miraclew <miraclew@gmail.com>
2015-05-31 14:15:48 +0800

    fix

--- pusherd/start.sh
+++ pusherd/start.sh
@@ -3,4 +3,4 @@ echo -e "package main\n\nconst BINARY_VERSION = \"$v\"" > v.go
 
 #go build && go install && rm pusherd && pusherd -rethinkDb="mercury" -rethinkAddr="192.168.33.10:28015" -redisAddr="192.168.33.10:6379"
 go build && go install && rm pusherd &&
-pusherd -rethinkDb="sun" -rethinkAddr="192.168.33.10:28015" -redisAddr="192.168.33.10:6379"
+pusherd -rethinkDb="sun" -rethinkAddr="192.168.33.10:28015" -redisAddr="192.168.33.10:6379" -wsAddr=":9004" -apiAddr="127.0.0.1:9005"
