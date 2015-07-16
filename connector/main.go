1d254a145a8a0987c03f06712560974ad1e1b54d
miraclew <miraclew@gmail.com>
2015-07-16 17:22:19 +0800

    add

--- connector/main.go
+++ connector/main.go
@@ -2,27 +2,60 @@ package main
 
 import (
 	"coding.net/miraclew/pusher/xlog"
-	logp "log"
+	"flag"
+	"fmt"
+	"github.com/op/go-logging"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 var (
-	log *xlog.Logger
+	log         *logging.Logger
+	showVersion = flag.Bool("version", false, "print version string")
+	wsIp        = flag.String("wsIp", "0.0.0.0", "<ip> to listen on for WebSocket clients")
+	wsPort      = flag.Int("wsPort", 9100, "<port> to listen on for WebSocket clients")
+	nodeId      = flag.Int("nodeId", 1, "id of the connector")
+	apiAddr     = flag.String("apiAddr", "127.0.0.1:9011", "<addr>:<port> to listen on for Http Api clients")
+	redisAddr   = flag.String("redisAddr", "127.0.0.1:6379", "<addr>:<port> (127.0.0.1:6379) redis address to connect")
 )
 
 func main() {
-	f, err := os.OpenFile("connector.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		logp.Fatalf("error opening file: %v", err)
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Println(Version("connector"))
 		return
 	}
-	defer f.Close()
 
-	log = xlog.NewNamed(f, "aa")
-	log.Debug = true
+	var err error
+	log, err = xlog.Open("connector")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer xlog.Close()
 
-	log.Debugf("debugf %s", "ddd")
-	log.Warnf("warn %s", "ddd")
-	log.Infof("info %s", "ddd")
-	log.Errorf("error %s", "ddd")
+	log.Info(Version("connector"))
+
+	exitChan := make(chan int)
+	signalChan := make(chan os.Signal, 1)
+	go func() {
+		<-signalChan
+		exitChan <- 1
+	}()
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	options := &AppOptions{
+		redisAddr: *redisAddr,
+		wsIp:      *wsIp,
+		wsPort:    *wsPort,
+		nodeId:    *nodeId,
+	}
+
+	app := NewApp(options)
+
+	app.Main()
+	<-exitChan
+	app.Exit()
 }
