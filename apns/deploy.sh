9fecda4b7ff96e86c324ccbdad1bc6967ef194da
miraclew <miraclew@gmail.com>
2015-07-25 20:07:25 +0800

    gix

--- apns/deploy.sh
+++ apns/deploy.sh
@@ -1,9 +1,9 @@
 mode=$1
 if [ "$mode" = "-prod" ]; then
         echo "prod mode"
-        scp ~/go/bin/linux_amd64/connector ubuntu@gx2:/data/pusher/connector_`date "+%m_%d_%H_%M_%S"`
+        scp ~/go/bin/linux_amd64/apns ubuntu@gx2:/data/pusher/apns_`date "+%m_%d_%H_%M_%S"`
 else
         echo "dev mode"
-        scp ~/go/bin/linux_amd64/connector ubuntu@gx1:/data/pusher/dev/connector
+        scp ~/go/bin/linux_amd64/apns ubuntu@gx1:/data/pusher/dev/apns
 fi
 
