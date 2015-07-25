3fa0cfd7d72a1b9d65a894dd96befc44fd6808cc
miraclew <miraclew@gmail.com>
2015-07-26 07:51:59 +0800

    improve

--- connector/deploy.sh
+++ connector/deploy.sh
@@ -4,6 +4,8 @@ if [ "$mode" = "-prod" ]; then
         scp ~/go/bin/linux_amd64/connector ubuntu@gx2:/data/pusher/connector_`date "+%m_%d_%H_%M_%S"`
 else
         echo "dev mode"
+        ssh ubuntu@gx1 -e "sudo supervisorctl stop connector:"
         scp ~/go/bin/linux_amd64/connector ubuntu@gx1:/data/pusher/dev/connector
+        ssh ubuntu@gx1 -e "sudo supervisorctl start connector:"
 fi
 
--- router/deploy.sh
+++ router/deploy.sh
@@ -4,6 +4,8 @@ if [ "$mode" = "-prod" ]; then
         scp ~/go/bin/linux_amd64/router ubuntu@gx2:/data/pusher/router_`date "+%m_%d_%H_%M_%S"`
 else
 		echo "dev mode"
+        ssh ubuntu@gx1 -e "sudo supervisorctl start router:"
         scp ~/go/bin/linux_amd64/router ubuntu@gx1:/data/pusher/dev/router
+        ssh ubuntu@gx1 -e "sudo supervisorctl start router:"
 fi
 
