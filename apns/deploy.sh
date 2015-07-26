316cc2764f27236235ecf7dc0cb28cb95cea99aa
miraclew <miraclew@gmail.com>
2015-07-26 18:16:52 +0800

    fix

--- apns/deploy.sh
+++ apns/deploy.sh
@@ -4,8 +4,8 @@ if [ "$mode" = "-prod" ]; then
         scp ~/go/bin/linux_amd64/apns ubuntu@gx2:/data/pusher/apns_`date "+%m_%d_%H_%M_%S"`
 else
         echo "dev mode"
-        ssh ubuntu@gx1 -e "sudo supervisorctl stop apns"
+        ssh ubuntu@gx1 sudo supervisorctl stop apns
         scp ~/go/bin/linux_amd64/apns ubuntu@gx1:/data/pusher/dev/apns
-        ssh ubuntu@gx1 -e "sudo supervisorctl start apns"
+        ssh ubuntu@gx1 sudo supervisorctl start apns
 fi
 
