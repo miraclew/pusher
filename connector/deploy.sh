cf5fe5b093ab3ae2601aec6b802f97f1474b9f17
miraclew <miraclew@gmail.com>
2015-11-02 18:06:23 +0800

    deploy app into bin directory

--- connector/deploy.sh
+++ connector/deploy.sh
@@ -4,11 +4,11 @@ if [[ "$host" == "" ]]; then
         echo "deploy host is required"
 else
         file_name=$app"_`date "+%m_%d_%H_%M_%S"`"
-        scp ~/go/bin/linux_amd64/$app ubuntu@$host:/data/pusher/$app/$file_name
+        scp ~/go/bin/linux_amd64/$app ubuntu@$host:/data/pusher/bin/$file_name
         echo "stop $app"
         ssh ubuntu@$host sudo supervisorctl stop $app:
-        ssh ubuntu@$host rm /data/pusher/$app/$app
-        ssh ubuntu@$host ln -s /data/pusher/$app/$file_name /data/pusher/$app/$app
+        ssh ubuntu@$host rm /data/pusher/bin/$app
+        ssh ubuntu@$host ln -s /data/pusher/bin/$file_name /data/pusher/bin/$app
         echo "start $app"
         ssh ubuntu@$host sudo supervisorctl start $app:
 fi
