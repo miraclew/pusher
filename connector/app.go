f06b18c851453f7957777f89b264188a8633ef33
miraclew <miraclew@gmail.com>
2015-11-18 17:04:12 +0800

    ff

--- connector/app.go
+++ connector/app.go
@@ -116,31 +116,33 @@ func (a *App) startConsumer() {
 }
 
 func (a *App) HandleMessage(message *nsq.Message) error {
-	// log.Debug("HandleMessage %s", string(message.Body))
-	cmd := &push.NodeCmd{}
-	err := json.Unmarshal(message.Body, cmd)
-	if err != nil {
-		log.Error("body malformed: body=%s err=%s", string(message.Body), err.Error())
-		return nil
-	}
-	if cmd.Cmd == push.NODE_CMD_PUSH {
-		body := &push.NodeCmdPush{}
-		err = json.Unmarshal(cmd.Body, body)
-
-		log.Debug("NodeCmdPush: msgId=%d receiverId: %d payload: %s", body.MsgId, body.ReceiverId, string(body.Payload))
-		conn := GetConnection(body.ReceiverId)
-		if conn != nil {
-			err := conn.WriteMessage(websocket.TextMessage, body.Payload)
-			if err != nil {
-				log.Error("WriteMessage err: %s", err.Error())
-				return nil
-			} else {
-				log.Info("Send OK msgId: %d => userId: %d", body.MsgId, body.ReceiverId)
-			}
-		} else {
-			log.Warning("User %d is not connected", body.ReceiverId)
+	log.Debug("HandleMessage")
+	go func() {
+		cmd := &push.NodeCmd{}
+		err := json.Unmarshal(message.Body, cmd)
+		if err != nil {
+			log.Error("body malformed: body=%s err=%s", string(message.Body), err.Error())
+			return
 		}
-	}
+		if cmd.Cmd == push.NODE_CMD_PUSH {
+			body := &push.NodeCmdPush{}
+			err = json.Unmarshal(cmd.Body, body)
+
+			log.Debug("NodeCmdPush: msgId=%d receiverId: %d payload: %s", body.MsgId, body.ReceiverId, string(body.Payload))
+			conn := GetConnection(body.ReceiverId)
+			if conn != nil {
+				err := conn.WriteMessage(websocket.TextMessage, body.Payload)
+				if err != nil {
+					log.Error("WriteMessage err: %s", err.Error())
+					return
+				} else {
+					log.Info("Send OK msgId: %d => userId: %d", body.MsgId, body.ReceiverId)
+				}
+			} else {
+				log.Warning("User %d is not connected", body.ReceiverId)
+			}
+		}
+	}()
 
 	return nil
 }
